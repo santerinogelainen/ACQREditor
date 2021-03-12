c132476680a0d781a5e8c6f09fefd5944e42ed5f
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2021-03-12 20:36:01 +0200

    Remove red line and fix camera resolution on scan page

--- ACQREditor/ACQREditor/Views/ScanPage.xaml.cs
+++ ACQREditor/ACQREditor/Views/ScanPage.xaml.cs
@@ -6,8 +6,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using ZXing;
+using ZXing.Mobile;
 
 namespace ACQREditor.Views
 {
@@ -88,13 +90,53 @@ namespace ACQREditor.Views
         {
             InitializeComponent();
 
+            // remove red line
+            var redLine = overlay.Children.FirstOrDefault(x => x.BackgroundColor == Color.Red); 
+
+            if (redLine != null)
+                overlay.Children.Remove(redLine);
+
+            scanView.Options.CameraResolutionSelector = SelectLowestResolutionMatchingDisplayAspectRatio;
             scanView.Options.PossibleFormats = new List<BarcodeFormat>
             {
                 BarcodeFormat.QR_CODE
             };
         }
 
-        private void scanView_OnScanResult(Result result)
+        public CameraResolution SelectLowestResolutionMatchingDisplayAspectRatio(List<CameraResolution> availableResolutions)
+        {
+            // https://dzone.com/articles/how-to-avoid-a-distorted-android-camera-preview-wi
+
+            CameraResolution result = null;
+
+            //a tolerance of 0.1 should not be recognizable for users
+            double aspectTolerance = 0.1;
+
+            //calculating our targetRatio
+            var targetRatio = DeviceDisplay.MainDisplayInfo.Height / DeviceDisplay.MainDisplayInfo.Width;
+            var targetHeight = DeviceDisplay.MainDisplayInfo.Height;
+            var minDiff = double.MaxValue;
+
+            //camera API lists all available resolutions from highest to lowest, perfect for us
+            //making use of this sorting, following code runs some comparisons to select the lowest resolution that matches the screen aspect ratio
+            //selecting the lowest makes QR detection actual faster most of the time
+            foreach (var r in availableResolutions)
+            {
+                //if current ratio is bigger than our tolerance, move on
+                //camera resolution is provided landscape ...
+                if (Math.Abs(((double)r.Width / r.Height) - targetRatio) > aspectTolerance)
+                    continue;
+                else
+                    if (Math.Abs(r.Height - targetHeight) < minDiff)
+                    minDiff = Math.Abs(r.Height - targetHeight);
+
+                result = r;
+            }
+
+            return result;
+        }
+
+        public void scanView_OnScanResult(Result result)
         {
             Device.BeginInvokeOnMainThread(async () =>
             {
